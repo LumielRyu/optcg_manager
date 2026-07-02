1b6f555ed620fbddedfbff8702b3b329a5f23a53
Otavio Augusto <otavio_cabalyes008@hotmail.com>
2026-07-02 15:09:30 -0300

    Use browser-like LigaOnePiece proxy headers

--- api/liga-one-piece.js
+++ api/liga-one-piece.js
@@ -117,11 +117,7 @@ async function resolveSource(query) {
     lastUrl = sourceUrl;
 
     const response = await fetch(sourceUrl, {
-      headers: {
-        Accept:
-          'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-        'User-Agent': 'Mozilla/5.0 OPTCG-Manager Proxy',
-      },
+      headers: browserLikeHeaders(sourceUrl),
     });
 
     lastStatus = response.status;
@@ -139,6 +135,33 @@ async function resolveSource(query) {
   error.statusCode = lastStatus === 200 ? 422 : lastStatus;
   error.sourceUrl = lastUrl;
   throw error;
+}
+
+function browserLikeHeaders(sourceUrl) {
+  return {
+    Accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Language': 'pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'no-cache',
+    Pragma: 'no-cache',
+    Referer: LIGA_BASE_URL,
+    Origin: 'https://www.ligaonepiece.com.br',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': sameLigaOrigin(sourceUrl) ? 'same-origin' : 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36',
+  };
+}
+
+function sameLigaOrigin(sourceUrl) {
+  try {
+    return new URL(sourceUrl).origin === new URL(LIGA_BASE_URL).origin;
+  } catch (_) {
+    return false;
+  }
 }
 
 function validateLigaUrl(rawUrl) {
